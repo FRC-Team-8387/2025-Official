2789632932cba3e3dd8b3b5d41182225b6a8e2e4
JosephV138 <josephd.404982@nv.ccsd.net>
2025-01-16 10:50:55 -0800

    more subsystem

---
+++ src/main/java/frc/robot/commands/scoring/ScoringCommand.java
@@ -0,0 +1,23 @@
+package frc.robot.commands.scoring;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.scoring.ScoringSubsystem;
+
+public class ScoringCommand extends Command {
+    private final ScoringSubsystem scoringSystem;
+    final double topRotation = 1000; //placeholder value, represents the rotations/degrees needed to reach the top
+
+    double targetPosition; // target proportion of the full height, from 0 (bottom) to 1 (top)
+
+    public ScoringCommand(ScoringSubsystem system, double target)
+    {
+        this.scoringSystem = system;
+        this.targetPosition = target;
+    }
+
+    @Override
+    public void execute() {
+      scoringSystem.moveTo(topRotation*targetPosition) //placeholder command, please make something similar in ScoringSubsystem
+  }
+
+}
--- src/main/java/frc/robot/commands/scoring/ScoringCommands.java
+++
@@ -1,8 +0,0 @@
-package frc.robot.commands.scoring;
-
-public class ScoringCommands {
-    private final ScoringSubsystem scoringSystem;
-    
-    double topRotations = 1000; //placeholder value
-    
-}
