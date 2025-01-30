6d1cd81ff5605d71f52d3df56bad8a94fc9bdaae
JosephV138 <josephd.404982@nv.ccsd.net>
2025-01-30 15:45:14 -0800

    added comments

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -18,15 +18,16 @@ import frc.robot.subsystems.swervedrive.SwerveSubsystem;
 import java.io.File;
 
 public class RobotContainer {
-    // Joysticks
+    // Joysticks for driver control
     private final Joystick leftStick = new Joystick(0);
     private final Joystick rightStick = new Joystick(1);
-    private boolean isHalfSpeed = false;
+    private boolean isHalfSpeed = false; // Toggle for speed reduction
 
-    // Subsystems
+    // Subsystems for drive and scoring
     private final SwerveSubsystem drivebase = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(), "swerve/8387"));
     private final ScoringSubsystem scoringSubsystem = new ScoringSubsystem();
 
+    // Drive command using joystick input
     AbsoluteDriveAdv closedAbsoluteDriveAdv = new AbsoluteDriveAdv(
         drivebase,
         () -> -MathUtil.applyDeadband(leftStick.getY() * (isHalfSpeed ? 0.5 : 1.0), OperatorConstants.LEFT_Y_DEADBAND),
@@ -38,6 +39,7 @@ public class RobotContainer {
         () -> false
     );
 
+    // Field-oriented drive command
     Command driveFieldOrientedDirectAngle = drivebase.driveCommand(
         () -> MathUtil.applyDeadband(leftStick.getY() * (isHalfSpeed ? 0.5 : 1.0) / 3.0, OperatorConstants.LEFT_Y_DEADBAND),
         () -> MathUtil.applyDeadband(leftStick.getX() * (isHalfSpeed ? 0.5 : 1.0) / 3.0, OperatorConstants.LEFT_X_DEADBAND),
@@ -45,12 +47,14 @@ public class RobotContainer {
         () -> rightStick.getY()
     );
 
+    // Angular velocity drive mode
     Command driveFieldOrientedAngularVelocity = drivebase.driveCommand(
         () -> MathUtil.applyDeadband(leftStick.getY() * (isHalfSpeed ? -0.5 : -1.0), OperatorConstants.LEFT_Y_DEADBAND),
         () -> MathUtil.applyDeadband(leftStick.getX() * (isHalfSpeed ? -0.5 : -1.0), OperatorConstants.LEFT_X_DEADBAND),
         () -> rightStick.getX() * (isHalfSpeed ? -0.5 : -1.0)
     );
 
+    // Simulation drive command
     Command driveFieldOrientedDirectAngleSim = drivebase.simDriveCommand(
         () -> MathUtil.applyDeadband(leftStick.getY() * (isHalfSpeed ? 0.5 : 1.0), OperatorConstants.LEFT_Y_DEADBAND),
         () -> MathUtil.applyDeadband(leftStick.getX() * (isHalfSpeed ? 0.5 : 1.0), OperatorConstants.LEFT_X_DEADBAND),
@@ -61,28 +65,35 @@ public class RobotContainer {
         configureBindings();
     }
 
+    // Configures joystick button bindings
     private void configureBindings() {
         if (!DriverStation.isTest()) {
+            // Toggle half-speed mode when left stick trigger is pressed
             new Trigger(leftStick::getTrigger).onTrue(Commands.runOnce(() -> isHalfSpeed = !isHalfSpeed));
             
+            // Right stick trigger to launch scoring mechanism
             new Trigger(rightStick::getTrigger).whileTrue(Commands.runOnce(() -> scoringSubsystem.launch()));
             
+            // Elevator movement controls
             new Trigger(() -> rightStick.getRawButton(2)).whileTrue(Commands.runOnce(() -> scoringSubsystem.pull()));
-            new Trigger(() -> rightStick.getRawButton(3)).whileTrue(Commands.runOnce(() -> scoringSubsystem.moveTo(ScoringSubsystem.MIN_ELEVATOR_HEIGHT)));
-            new Trigger(() -> rightStick.getRawButton(4)).whileTrue(Commands.runOnce(() -> scoringSubsystem.moveTo(ScoringSubsystem.MAX_ELEVATOR_HEIGHT*0.25)));
-            new Trigger(() -> rightStick.getRawButton(5)).whileTrue(Commands.runOnce(() -> scoringSubsystem.moveTo(ScoringSubsystem.MAX_ELEVATOR_HEIGHT*0.50)));
-            new Trigger(() -> rightStick.getRawButton(6)).whileTrue(Commands.runOnce(() -> scoringSubsystem.moveTo(ScoringSubsystem.MAX_ELEVATOR_HEIGHT*1.0)));
+            new Trigger(() -> rightStick.getRawButton(3)).whileTrue(Commands.runOnce(() -> scoringSubsystem.moveTo(0)));
+            new Trigger(() -> rightStick.getRawButton(4)).whileTrue(Commands.runOnce(() -> scoringSubsystem.moveTo(25)));
+            new Trigger(() -> rightStick.getRawButton(5)).whileTrue(Commands.runOnce(() -> scoringSubsystem.moveTo(50)));
+            new Trigger(() -> rightStick.getRawButton(6)).whileTrue(Commands.runOnce(() -> scoringSubsystem.moveTo(100)));
         }
     }
 
+    // Returns the autonomous command
     public Command getAutonomousCommand() {
         return drivebase.getAutonomousCommand("New Auto");
     }
 
+    // Reconfigures joystick bindings
     public void setDriveMode() {
         configureBindings();
     }
 
+    // Sets the motor brake mode
     public void setMotorBrake(boolean brake) {
         drivebase.setMotorBrake(brake);
     }
