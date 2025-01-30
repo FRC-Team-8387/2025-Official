37137704587b6e2e4f0d2cb16adc21bc82a60ea4
JosephV138 <josephd.404982@nv.ccsd.net>
2025-01-30 15:55:03 -0800

    removed non-existent import

--- src/main/java/frc/robot/subsystems/scoring/ScoringSubsystem.java
+++ src/main/java/frc/robot/subsystems/scoring/ScoringSubsystem.java
@@ -1,7 +1,6 @@
 package frc.robot.subsystems.scoring;
 
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.PWMVictorSPX;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
