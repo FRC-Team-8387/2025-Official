715e5f2d7aa990788632319b9fd0432fffb699bb
JosephV138 <josephd.404982@nv.ccsd.net>
2025-02-20 10:18:57 -0800

    swapped back to xbox controller
    not really anything much, Lance needs to push his changes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,3 +1,9 @@
+
+
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot;
 
 import edu.wpi.first.math.MathUtil;
@@ -6,96 +12,141 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Filesystem;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.swervedrive.drivebase.AbsoluteDriveAdv;
-import frc.robot.subsystems.scoring.ScoringSubsystem;
 import frc.robot.subsystems.swervedrive.SwerveSubsystem;
 import java.io.File;
 
-public class RobotContainer {
-    // Joysticks for driver control
-    private final Joystick leftStick = new Joystick(0);
-    private final Joystick rightStick = new Joystick(1);
-    private boolean isHalfSpeed = false; // Toggle for speed reduction
 
-    // Subsystems for drive and scoring
-    private final SwerveSubsystem drivebase = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(), "swerve/8387"));
-    private final ScoringSubsystem scoringSubsystem = new ScoringSubsystem();
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a "declarative" paradigm, very
+ * little robot logic should actually be handled in the {@link Robot} periodic methods (other than the scheduler calls).
+ * Instead, the structure of the robot (including subsystems, commands, and trigger mappings) should be declared here.
+ */
+public class RobotContainer
+{
 
-    // Drive command using joystick input
-    AbsoluteDriveAdv closedAbsoluteDriveAdv = new AbsoluteDriveAdv(
-        drivebase,
-        () -> -MathUtil.applyDeadband(leftStick.getY() * (isHalfSpeed ? 0.5 : 1.0), OperatorConstants.LEFT_Y_DEADBAND),
-        () -> -MathUtil.applyDeadband(leftStick.getX() * (isHalfSpeed ? 0.5 : 1.0), OperatorConstants.LEFT_X_DEADBAND),
-        () -> -MathUtil.applyDeadband(rightStick.getX() * (isHalfSpeed ? 0.5 : 1.0), OperatorConstants.RIGHT_X_DEADBAND),
-        leftStick::getTrigger,
-        () -> false,
-        () -> false,
-        () -> false
-    );
+  // Replace with CommandPS4Controller or CommandJoystick if needed
+  final         CommandXboxController driverXbox = new CommandXboxController(0);
+  // The robot's subsystems and commands are defined here...
+  private final SwerveSubsystem       drivebase  = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(),
+                                                                                "swerve/8387"));
+  // Applies deadbands and inverts controls because joysticks
+  // are back-right positive while robot
+  // controls are front-left positive
+  // left stick controls translation
+  // right stick controls the rotational velocity 
+  // buttons are quick rotation positions to different ways to face
+  // WARNING: default buttons are on the same buttons as the ones defined in configureBindings
+  AbsoluteDriveAdv closedAbsoluteDriveAdv = new AbsoluteDriveAdv(drivebase,
+                                                                 () -> -MathUtil.applyDeadband(driverXbox.getLeftY(),
+                                                                                               OperatorConstants.LEFT_Y_DEADBAND),
+                                                                 () -> -MathUtil.applyDeadband(driverXbox.getLeftX(),
+                                                                                               OperatorConstants.LEFT_X_DEADBAND),
+                                                                 () -> -MathUtil.applyDeadband(driverXbox.getRightX(),
+                                                                                               OperatorConstants.RIGHT_X_DEADBAND),
+                                                                 driverXbox.getHID()::getYButtonPressed,
+                                                                 driverXbox.getHID()::getAButtonPressed,
+                                                                 driverXbox.getHID()::getXButtonPressed,
+                                                                 driverXbox.getHID()::getBButtonPressed);
 
-    // Field-oriented drive command
-    Command driveFieldOrientedDirectAngle = drivebase.driveCommand(
-        () -> MathUtil.applyDeadband(leftStick.getY() * (isHalfSpeed ? 0.5 : 1.0) / 3.0, OperatorConstants.LEFT_Y_DEADBAND),
-        () -> MathUtil.applyDeadband(leftStick.getX() * (isHalfSpeed ? 0.5 : 1.0) / 3.0, OperatorConstants.LEFT_X_DEADBAND),
-        () -> rightStick.getX(),
-        () -> rightStick.getY()
-    );
+  // Applies deadbands and inverts controls because joysticks
+  // are back-right positive while robot
+  // controls are front-left positive
+  // left stick controls translation
+  // right stick controls the desired angle NOT angular rotation
+  Command driveFieldOrientedDirectAngle = drivebase.driveCommand(
+      () -> MathUtil.applyDeadband(driverXbox.getLeftY()/3.0, OperatorConstants.LEFT_Y_DEADBAND),
+      () -> MathUtil.applyDeadband(driverXbox.getLeftX()/3.0, OperatorConstants.LEFT_X_DEADBAND),
+      () -> driverXbox.getRightX(),
+      () -> driverXbox.getRightY());
 
-    // Angular velocity drive mode
-    Command driveFieldOrientedAngularVelocity = drivebase.driveCommand(
-        () -> MathUtil.applyDeadband(leftStick.getY() * (isHalfSpeed ? -0.5 : -1.0), OperatorConstants.LEFT_Y_DEADBAND),
-        () -> MathUtil.applyDeadband(leftStick.getX() * (isHalfSpeed ? -0.5 : -1.0), OperatorConstants.LEFT_X_DEADBAND),
-        () -> rightStick.getX() * (isHalfSpeed ? -0.5 : -1.0)
-    );
+  // Applies deadbands and inverts controls because joysticks
+  // are back-right positive while robot
+  // controls are front-left positive
+  // left stick controls translation
+  // right stick controls the angular velocity of the robot
+  Command driveFieldOrientedAnglularVelocity = drivebase.driveCommand(
+      () -> MathUtil.applyDeadband(driverXbox.getLeftY() * -1, OperatorConstants.LEFT_Y_DEADBAND),
+      () -> MathUtil.applyDeadband(driverXbox.getLeftX() * -1, OperatorConstants.LEFT_X_DEADBAND),
+      () -> driverXbox.getRightX() * -1);
 
-    // Simulation drive command
-    Command driveFieldOrientedDirectAngleSim = drivebase.simDriveCommand(
-        () -> MathUtil.applyDeadband(leftStick.getY() * (isHalfSpeed ? 0.5 : 1.0), OperatorConstants.LEFT_Y_DEADBAND),
-        () -> MathUtil.applyDeadband(leftStick.getX() * (isHalfSpeed ? 0.5 : 1.0), OperatorConstants.LEFT_X_DEADBAND),
-        () -> rightStick.getRawAxis(2)
-    );
+  Command driveFieldOrientedDirectAngleSim = drivebase.simDriveCommand(
+      () -> MathUtil.applyDeadband(driverXbox.getLeftY(), OperatorConstants.LEFT_Y_DEADBAND),
+      () -> MathUtil.applyDeadband(driverXbox.getLeftX(), OperatorConstants.LEFT_X_DEADBAND),
+      () -> driverXbox.getRawAxis(2));
 
-    public RobotContainer() {
-        configureBindings();
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
+  public RobotContainer()
+  {
+    // Configure the trigger bindings
+    configureBindings();
+  }
+
+  /**
+   * Use this method to define your trigger->command mappings. Triggers can be created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary predicate, or via the
+   * named factories in {@link edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
+   * {@link CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller PS4}
+   * controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight joysticks}.
+   */
+  private void configureBindings()
+  {
+    // Old bindings for an xbox controller; we need to make new ones for the joysticks.
+    if (DriverStation.isTest())
+    {
+      driverXbox.b().whileTrue(drivebase.sysIdDriveMotorCommand());
+      driverXbox.x().whileTrue(Commands.runOnce(drivebase::lock, drivebase).repeatedly());
+      driverXbox.y().whileTrue(drivebase.driveToDistanceCommand(1.0, 0.2));
+      driverXbox.start().onTrue((Commands.runOnce(drivebase::zeroGyro)));
+      driverXbox.back().whileTrue(drivebase.centerModulesCommand());
+      driverXbox.leftBumper().onTrue(Commands.none());
+      driverXbox.rightBumper().onTrue(Commands.none());
+      drivebase.setDefaultCommand(
+          !RobotBase.isSimulation() ? driveFieldOrientedAnglularVelocity : driveFieldOrientedDirectAngleSim);
+    } else
+    {
+      driverXbox.a().onTrue((Commands.runOnce(drivebase::zeroGyro)));
+      driverXbox.x().onTrue(Commands.runOnce(drivebase::addFakeVisionReading));
+      driverXbox.b().whileTrue(
+          Commands.deferredProxy(() -> drivebase.driveToPose(
+                                     new Pose2d(new Translation2d(4, 4), Rotation2d.fromDegrees(0)))
+                                ));
+      driverXbox.y().whileTrue(drivebase.aimAtSpeaker(2));
+      driverXbox.start().whileTrue(Commands.none());
+      driverXbox.back().whileTrue(Commands.none());
+      driverXbox.leftBumper().whileTrue(Commands.runOnce(drivebase::lock, drivebase).repeatedly());
+      driverXbox.rightBumper().onTrue(Commands.none());
+      drivebase.setDefaultCommand(
+          !RobotBase.isSimulation() ? driveFieldOrientedDirectAngle : driveFieldOrientedDirectAngleSim);
     }
+  }
 
-    // Configures joystick button bindings
-    private void configureBindings() {
-        if (!DriverStation.isTest()) {
-            // Toggle half-speed mode when left stick trigger is pressed
-            new Trigger(leftStick::getTrigger).onTrue(Commands.runOnce(() -> isHalfSpeed = !isHalfSpeed));
-            
-            // Right stick trigger to launch scoring mechanism
-            new Trigger(rightStick::getTrigger).whileTrue(Commands.runOnce(() -> scoringSubsystem.launch()));
-            
-            // Elevator movement controls - current moveTo parameters are placeholders
-            new Trigger(() -> rightStick.getRawButton(2)).whileTrue(Commands.runOnce(() -> scoringSubsystem.pull()));
-            new Trigger(() -> rightStick.getRawButton(3)).whileTrue(Commands.runOnce(() -> scoringSubsystem.moveTo(0)));
-            new Trigger(() -> rightStick.getRawButton(4)).whileTrue(Commands.runOnce(() -> scoringSubsystem.moveTo(25)));
-            new Trigger(() -> rightStick.getRawButton(5)).whileTrue(Commands.runOnce(() -> scoringSubsystem.moveTo(50)));
-            new Trigger(() -> rightStick.getRawButton(6)).whileTrue(Commands.runOnce(() -> scoringSubsystem.moveTo(100)));
-        }
-    }
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand()
+  {
+    // An example command will be run in autonomous
+    return drivebase.getAutonomousCommand("New Auto");
+  }
 
-    // Returns the autonomous command
-    public Command getAutonomousCommand() {
-        return drivebase.getAutonomousCommand("New Auto");
-    }
+  public void setDriveMode()
+  {
+    configureBindings();
+  }
 
-    // Reconfigures joystick bindings
-    public void setDriveMode() {
-        configureBindings();
-    }
-
-    // Sets the motor brake mode
-    public void setMotorBrake(boolean brake) {
-        drivebase.setMotorBrake(brake);
-    }
+  public void setMotorBrake(boolean brake)
+  {
+    drivebase.setMotorBrake(brake);
+  }
 }
